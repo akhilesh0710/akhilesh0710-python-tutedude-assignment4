241550fe5a02eb53e4544c7f80c211e67792eed9
akhilesh0710 <84137682+akhilesh0710@users.noreply.github.com>
2025-08-22 19:23:34 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,6 +1,10 @@
 try:
+    # Open sample.txt in read mode
     with open('sample.txt', 'r') as f:
+        # Print each line with line number
         for idx, line in enumerate(f, start=1):
             print(f"Line {idx} : {line.strip()}")
+
+# Show error if file not found
 except FileNotFoundError:
     print("Error: The file 'sample.txt' was not found.")
